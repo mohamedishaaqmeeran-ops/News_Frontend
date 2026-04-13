4a59659999e2b6f837bdaf173b3057d21d462c33
unknown <mohamedishaaqmeeran@gmail.com>
2026-04-13 17:58:48 +0530

    completed

--- newsapp/src/instances/instance.js
+++ newsapp/src/instances/instance.js
@@ -4,7 +4,7 @@ const baseURL = 'https://news-backend-17sl.onrender.com/api/v1';
 
 const instance = axios.create({
     baseURL: baseURL,
-    timeout: 1000000,
+    timeout: 10000,
     headers: {
         "Content-Type": "application/json"
     }
